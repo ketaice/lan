88ee0ca9886e94d6645594399b3a8bf44144e8ba
Keta Guo <Keta Guo>
2017-06-11 18:51:43 +0800

    Add device database testcast.

---
+++ test/models/device_spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var Bluebird = require('bluebird');
+var expect = require('chai').expect;
+
+describe('Device Model', function () {
+    var models = null;
+    beforeEach(function () {
+        models = require('../../models');
+
+        return Bluebird.all([
+			models.Device.destroy({truncate: true})
+		]);
+    });
+
+    it('should create device successful', function (done) {
+        models.Device.create({
+            devid: '201700100001',
+            password: '12345678',
+            expiration: '2027-01-01',
+            ower: 'keta'
+        }).then(function () {
+            models.Device.findOne({where: {devid: '201700100001'}}).then(function(device){
+                if (device.devid === '201700100001') {
+                    done();
+                }
+            })
+        })
+    });
+});
