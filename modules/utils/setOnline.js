2d03cac9d923f532e8bdaa2cc8d526b8766a508e
Keta <keta.guo@foxmail.com>
2017-06-24 16:38:10 +0800

    Change key.

--- modules/utils/setOnline.js
+++ modules/utils/setOnline.js
@@ -2,13 +2,14 @@ var request = require('request');
 var config = require('config');
 
 'use strict';
-module.exports = function(devid, online) {
+module.exports = function(devid, key) {
     var url = 'http://localhost:' + config.get('port.http');
     url += '/v1/devices/' + devid.toString();
 
-    return request.put(url, {online: online}, function (err, res){
-        if (res.statusCode === 200) {
-            console.log(devid + ' set online to ' + online);
+    return request.put(url, {online: key}, 
+    function (error, response){
+        if (!error && response.statusCode === 200) {
+            console.log(devid + ' set online to ' + key);
         }
     })
 };
