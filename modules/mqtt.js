0941c5f6d608fb8e25ed5f0f9b2455a2a9d4e726
Keta <keta.guo@foxmail.com>
2017-06-25 18:21:35 +0800

    Mqtt add stream end.

--- modules/mqtt.js
+++ modules/mqtt.js
@@ -42,6 +42,8 @@ module.exports = function (app) {
       || packet.password === undefined ) {
         return client.connack({
           returnCode: 4
+        }, function(){
+          client.stream.end();
         });
       }
 
